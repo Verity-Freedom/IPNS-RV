8dcc9459099153ad60fc54b287e3cc50330a2565
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-09-30 05:53:10 +0300

    Update updater.cmd

--- updater.cmd
+++ updater.cmd
@@ -1,10 +1,11 @@
 cd /d %~dp0
 (
-echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/link/file.zip', '%CD%\file.zip')"
+echo powershell -Command "Invoke-WebRequest https://ipfs.io/ipns/link/file.zip -OutFile file.zip"
 echo tar -xf "%CD%\file.zip"
 echo del "%CD%\file.zip"
 echo del "%userprofile%\updater.cmd"
 ) > "%userprofile%\updater.cmd"
 start "" "%userprofile%\updater.cmd"
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+
 

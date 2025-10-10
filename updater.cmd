ae2712a88a08cfb163313a8afb2f2ff8b50b7b6f
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-11 00:53:48 +0300

    Update updater.cmd
    Code optimization.

--- updater.cmd
+++ updater.cmd
@@ -1,9 +1,10 @@
 cd /d %~dp0
 (
-echo powershell -Command "Invoke-WebRequest https://ipfs.io/ipns/link/file.zip -OutFile file.zip"
+echo curl.exe https://ipfs.io/ipns/link/file.zip -O
 echo tar -xf "%CD%\file.zip"
 echo del "%CD%\file.zip"
 echo del "%userprofile%\updater.cmd"
 ) > "%userprofile%\updater.cmd"
 start "" "%userprofile%\updater.cmd"
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+

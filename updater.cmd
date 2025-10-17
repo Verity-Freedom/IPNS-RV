a95223f7995bae943368a10c70cd10c098a2a072
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-17 17:43:32 +0300

    Update updater.cmd

--- updater.cmd
+++ updater.cmd
@@ -1,9 +1,11 @@
 cd /d %~dp0
 (
+echo @echo off
 echo curl.exe https://ipfs.io/ipns/link/file.zip -O
 echo tar -xf "%CD%\file.zip"
 echo del "%CD%\file.zip"
-echo del "%userprofile%\updater.cmd"
+echo del "%userprofile%\updater.cmd" >nul
+echo exit
 ) > "%userprofile%\updater.cmd"
 start "" "%userprofile%\updater.cmd"
 for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)

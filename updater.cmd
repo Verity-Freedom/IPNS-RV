092178c1317a0f3df71a3fa38a31832360cd8a66
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-31 18:48:55 +0300

    Update updater.cmd
    Added user-settings saving implementation.

--- updater.cmd
+++ updater.cmd
@@ -4,8 +4,11 @@ echo @echo off
 echo curl.exe https://ipfs.io/ipns/link/file.zip -O
 echo tar -xf "%CD%\file.zip"
 echo del "%CD%\file.zip"
+echo xcopy "%userprofile%\user-config" "%CD%\user-config" /i /e
+echo rmdir "%userprofile%\user-config" /s /q
 echo start cmd /C del "%userprofile%\updater.cmd"
 echo exit
 ) > "%userprofile%\updater.cmd"
+xcopy "%CD%\user-config" "%userprofile%\user-config" /i /e
 start "" "%userprofile%\updater.cmd"
 rmdir "%CD%" /s /q

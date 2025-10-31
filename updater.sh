298d8bf1c31e1254099f2ecd039e39e9e5f55012
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-10-31 20:01:17 +0300

    Update updater.sh
    Added user-settings saving implementation.

--- updater.sh
+++ updater.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+cp -r ./user-config ~/user-config
 rm -r *
 curl https://ipfs.io/ipns/link/file.zip -O
 unzip ./file.zip
 rm ./file.zip
+cp -r ~/user-config ./user-config
+rm -r ~/user-config

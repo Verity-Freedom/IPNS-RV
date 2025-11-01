12e75b319cd5529fb43d4124f1827c0263336958
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-01 12:46:01 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-cp -r ./user-config ~/user-config
+cp -r ./data ~/data
 rm -r *
 curl https://ipfs.io/ipns/link/file.zip -O
 unzip ./file.zip
 rm ./file.zip
-cp -r ~/user-config ./
-rm -r ~/user-config
+cp -r ~/data ./
+rm -r ~/data

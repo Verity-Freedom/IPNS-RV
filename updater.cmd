05ab017f9998bd875ecb8d172fb64427f51660e5
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-01 12:45:18 +0300

    Update updater.cmd

--- updater.cmd
+++ updater.cmd
@@ -3,11 +3,11 @@ cd /d "%~dp0"
 echo curl.exe https://ipfs.io/ipns/link/file.zip -O
 echo tar -xf "%CD%\file.zip"
 echo del "%CD%\file.zip"
-echo xcopy "%userprofile%\user-config" "%CD%\user-config" /i /e
-echo rmdir "%userprofile%\user-config" /s /q
+echo xcopy "%userprofile%\data" "%CD%\data" /i /e
+echo rmdir "%userprofile%\data" /s /q
 echo start cmd /C del "%userprofile%\updater.cmd"
 echo exit
 ) > "%userprofile%\updater.cmd"
-xcopy "%CD%\user-config" "%userprofile%\user-config" /i /e
+xcopy "%CD%\data" "%userprofile%\data" /i /e
 start "" "%userprofile%\updater.cmd"
 rmdir "%CD%" /s /q

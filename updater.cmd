0c6502585759f322dc48051b1cdc7de3022c953d
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-17 13:44:36 +0300

    Update updater.cmd
    Introduce Win 7 support!

--- updater.cmd
+++ updater.cmd
@@ -1,13 +1,25 @@
 cd /d "%~dp0"
 (
-echo curl.exe "https://ipfs.io/ipns/link/file.zip" -O
-echo tar -xf "%CD%\file.zip"
+echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/link/file.zip', '%CD%\file.zip')"
+echo start "" "%userprofile%\extractor.vbs"
+echo exit
+) > "%userprofile%\updater.cmd"
+(
+echo ZipFile="%CD%\file.zip"
+echo ExtractTo="%CD%"
+echo set objShell = CreateObject("Shell.Application"^)
+echo set FilesInZip=objShell.NameSpace(ZipFile^).items
+echo objShell.NameSpace(ExtractTo^).CopyHere(FilesInZip^)
+echo CreateObject("WScript.Shell"^).Run "%userprofile%\cleaner.cmd"
+) > "%userprofile%\extractor.vbs"
+(
 echo del "%CD%\file.zip"
 echo xcopy "%userprofile%\data" "%CD%\data" /i /e
 echo rmdir "%userprofile%\data" /s /q
 echo start cmd /C del "%userprofile%\updater.cmd"
-echo exit
-) > "%userprofile%\updater.cmd"
+echo start cmd /C del "%userprofile%\extractor.vbs"
+echo start cmd /C del "%userprofile%\cleaner.cmd"
+) > "%userprofile%\cleaner.cmd"
 xcopy "%CD%\data" "%userprofile%\data" /i /e
 start "" "%userprofile%\updater.cmd"
 rmdir "%CD%" /s /q

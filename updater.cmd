14c22677c9e03d73f1b391ac02980a3ff1c4d4bf
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-18 08:18:40 +0300

    Update updater.cmd
    Support for cyrillic.

--- updater.cmd
+++ updater.cmd
@@ -1,16 +1,17 @@
-cd /d "%~dp0"
+cd /d %~dp0
 (
 echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/link/file.zip', '%CD%\file.zip')"
 echo start "" "%userprofile%\extractor.vbs"
 echo exit
 ) > "%userprofile%\updater.cmd"
 (
-echo ZipFile="%CD%\file.zip"
-echo ExtractTo="%CD%"
-echo set objShell = CreateObject("Shell.Application"^)
-echo set FilesInZip=objShell.NameSpace(ZipFile^).items
-echo objShell.NameSpace(ExtractTo^).CopyHere(FilesInZip^)
-echo CreateObject("WScript.Shell"^).Run "%userprofile%\cleaner.cmd"
+(
+cmd /u /c echo ZipFile="%CD%\file.zip"
+cmd /u /c echo ExtractTo="%CD%"
+cmd /u /c echo set objShell = CreateObject("Shell.Application"^)
+cmd /u /c echo set FilesInZip=objShell.NameSpace(ZipFile^).items
+cmd /u /c echo objShell.NameSpace(ExtractTo^).CopyHere(FilesInZip^)
+cmd /u /c echo CreateObject("WScript.Shell"^).Run "%userprofile%\cleaner.cmd"
 ) > "%userprofile%\extractor.vbs"
 (
 echo del "%CD%\file.zip"

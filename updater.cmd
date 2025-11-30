37aa4c059bc8e292d2e9af54bc538cac1d5e59e9
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-11-30 23:25:16 +0300

    Update updater.cmd
    Use temp instead of userprofile

--- updater.cmd
+++ updater.cmd
@@ -2,21 +2,21 @@ cd /d "%~dp0"
 if "%CD:~-1%" == "\" (set "WAY=%CD:~0,-1%") else set "WAY=%CD%"
 (
 echo powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/link/file.zip', '%WAY%\file.zip')"
-echo cscript "%userprofile%\extractor.vbs"
+echo cscript "%temp%\extractor.vbs"
 echo exit
-)>"%userprofile%\updater.cmd"
+)>"%temp%\updater.cmd"
 (
 cmd /u /c echo CreateObject("Shell.Application"^).NameSpace("%CD%"^).CopyHere(CreateObject("Shell.Application"^).NameSpace("%WAY%\file.zip"^).items^)
-cmd /u /c echo CreateObject("WScript.Shell"^).Run "%userprofile%\cleaner.cmd"
-)>"%userprofile%\extractor.vbs"
+cmd /u /c echo CreateObject("WScript.Shell"^).Run "%temp%\cleaner.cmd"
+)>"%temp%\extractor.vbs"
 (
 echo del "%WAY%\file.zip"
-echo xcopy "%userprofile%\data" "%CD%\data" /i /e
-echo rmdir "%userprofile%\data" /s /q
-echo cmd /c del "%userprofile%\updater.cmd"
-echo cmd /c del "%userprofile%\extractor.vbs"
-echo cmd /c del "%userprofile%\cleaner.cmd"
-)>"%userprofile%\cleaner.cmd"
-xcopy "%CD%\data" "%userprofile%\data" /i /e
-start "" "%userprofile%\updater.cmd"
+echo xcopy "%temp%\data" "%CD%\data" /i /e
+echo rmdir "%temp%\data" /s /q
+echo cmd /c del "%temp%\updater.cmd"
+echo cmd /c del "%temp%\extractor.vbs"
+echo cmd /c del "%temp%\cleaner.cmd"
+)>"%temp%\cleaner.cmd"
+xcopy "%CD%\data" "%temp%\data" /i /e
+start "" "%temp%\updater.cmd"
 rmdir "%CD%" /s /q

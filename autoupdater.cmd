faf359c4159095acd834109b32f2384bf498047c
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-01 21:05:27 +0300

    Update autoupdater.cmd
    Much better code

--- autoupdater.cmd
+++ autoupdater.cmd
@@ -1,17 +1,19 @@
 @echo off
+setlocal EnableDelayedExpansion
 for %%I in (VERSION*) do set "UPD=%%~nxI"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')" >nul
 if %errorlevel% NEQ 0 (
+choice /m "Update is available. Do you want to update"
+if !errorlevel! EQU 2 GOTO Skip
 echo @echo off>"%temp%\autoupdater.cmd"
 echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
 echo cls>>"%temp%\autoupdater.cmd"
 echo echo If you see this message, then the automatic update is most likely successful. You can wait or press any button to start the program.>>"%temp%\autoupdater.cmd"
 echo timeout /t 15>>"%temp%\autoupdater.cmd"
 echo call "%CD%\program.exe">>"%temp%\autoupdater.cmd"
-echo exit>>"%temp%\autoupdater.cmd"
+echo del "%temp%\autoupdater.cmd" ^& exit>>"%temp%\autoupdater.cmd"
 start "" "%temp%\autoupdater.cmd"
 exit
-) else (
-del "%temp%\%UPD%"
-del "%temp%\autoupdater.cmd" >nul 2>&1
 )
+del "%temp%\%UPD%"
+:Skip

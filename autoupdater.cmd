0e7e16043eda9d0aa68877bb23984fc062d79e53
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-04 06:02:30 +0300

    Update autoupdater.cmd
    It is much harder to work with services both on Windows and Linux so it is logical to use the services dedicated examples

--- autoupdater.cmd
+++ autoupdater.cmd
@@ -1,19 +1,25 @@
-cd /d "%~dp0"
 setlocal EnableDelayedExpansion
 for %%I in (VERSION*) do set "UPD=%%~nxI"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')" >nul
 if %errorlevel% NEQ 0 (
 choice /m "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Do you want to update"
 if !errorlevel! EQU 2 GOTO Skip
+sc query "Service" >nul
+if !errorlevel! EQU 0 set "CHECK=0" & GOTO Service
+:Loop
 echo @echo off>"%temp%\autoupdater.cmd"
 echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
 echo cls>>"%temp%\autoupdater.cmd"
 echo :Wait>>"%temp%\autoupdater.cmd"
 echo if not exist "%CD%\file.any" GOTO Wait>>"%temp%\autoupdater.cmd"
-echo call "%CD%\program.exe">>"%temp%\autoupdater.cmd"
+echo if "%CHECK%" NEQ "1" call "%CD%\service.exe">>"%temp%\autoupdater.cmd"
 echo del "%temp%\autoupdater.cmd" ^& exit>>"%temp%\autoupdater.cmd"
 start "" "%temp%\autoupdater.cmd"
 exit
 )
 del "%temp%\%UPD%"
 :Skip
+...
+:Service
+...
+If "%CHECK%"=="0" set "CHECK=1" & GOTO Loop

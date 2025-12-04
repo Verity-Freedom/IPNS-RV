153bbfa1f12629245579944d42631729f0b28c88
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-04 14:48:02 +0300

    Create autoupdater-program.cmd

---
+++ autoupdater-program.cmd
@@ -0,0 +1,19 @@
+cd /d "%~dp0"
+setlocal EnableDelayedExpansion
+for %%I in (VERSION*) do set "UPD=%%~nxI"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/link/%UPD%', '%temp%\%UPD%')" >nul
+if %errorlevel% NEQ 0 (
+choice /m "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Do you want to update"
+if !errorlevel! EQU 2 GOTO Skip
+echo @echo off>"%temp%\autoupdater.cmd"
+echo call "%CD%\updater.cmd">>"%temp%\autoupdater.cmd"
+echo cls>>"%temp%\autoupdater.cmd"
+echo :Wait>>"%temp%\autoupdater.cmd"
+echo if not exist "%CD%\file.any" GOTO Wait>>"%temp%\autoupdater.cmd"
+echo call "%CD%\program.exe">>"%temp%\autoupdater.cmd"
+echo del "%temp%\autoupdater.cmd" ^& exit>>"%temp%\autoupdater.cmd"
+start "" "%temp%\autoupdater.cmd"
+exit
+)
+del "%temp%\%UPD%"
+:Skip

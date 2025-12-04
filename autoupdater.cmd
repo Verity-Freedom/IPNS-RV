d98c4c8416f048251f7f2b4d0c109039a6b85f39
Verity-Freedom <143919205+Verity-Freedom@users.noreply.github.com>
2025-12-04 06:04:38 +0300

    Update autoupdater.cmd

--- autoupdater.cmd
+++ autoupdater.cmd
@@ -1,7 +1,7 @@
 cd /d "%~dp0"
 setlocal EnableDelayedExpansion
 for %%I in (VERSION*) do set "UPD=%%~nxI"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/k51qzi5uqu5dldod6robuflgitvj276br0xye3adipm3kc0bh17hfiv1e0hnp4/%UPD%', '%temp%\%UPD%')" >nul
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ipfs.io/ipns/link/%UPD%', '%temp%\%UPD%')" >nul
 if %errorlevel% NEQ 0 (
 choice /m "The local version does not match the latest version. It means that update is available, but in edge cases marks accessibility issues. Do you want to update"
 if !errorlevel! EQU 2 GOTO Skip
